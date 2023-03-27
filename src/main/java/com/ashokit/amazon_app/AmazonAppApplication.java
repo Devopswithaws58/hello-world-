b03a8867ad246041658a1369c7aa78d5e8d179d0
Saidarao Devarakonda <devopswithaws58@gmail.com>
2023-03-27 19:22:57 +0530

    amazon app added

---
+++ src/main/java/com/ashokit/amazon_app/AmazonAppApplication.java
@@ -0,0 +1,13 @@
+package com.ashokit.amazon_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazonAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ashokit/amazon_app/AmazonAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashokit.amazon_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
